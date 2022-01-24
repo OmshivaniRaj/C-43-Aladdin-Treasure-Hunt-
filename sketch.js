8af0f8ed8e97ce1e7ba9fadfda7d3b468e70108d
OmshivaniRaj <84126951+OmshivaniRaj@users.noreply.github.com>
2022-01-24 11:20:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+
+function preload() {
+  bg = loadImage("aladdin_cave.jpg");
+  bg2 = loadImage("treasure.jpg");
+  bgSong = loadSound("aladdin theme song 1.mp3");
+}
+
+function setup() {
+  var canvas =createCanvas(windowWidth,windowHeight);
+  system = new System()
+  security = new Security()
+  bgSong.play();
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+
+  if(score === 3) {
+    clear()
+    background(bg2)
+    strokeWeight(4);
+    stroke("blue");
+    fill("black");
+    textSize(40);
+    text("👍❤️TREASURE UNLOCKED❤️👍",250, 200);
+
+  }
+
+  drawSprites()
+}
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
+}
